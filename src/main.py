ca369afa0aae9526803ba73e5999ce08b1e2fc7e
JustDataSimple <JustDataSimple@proton.me>
2025-03-30 23:36:36 -0500

    created main

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from statementReader import statement_reader
+
+
+pdf_text = statement_reader("C:\\users\\antuj\\Finance\\Statements\\\chime\\Tamara_Russell_Checking_eStatement-1.pdf")
+print(pdf_text)
